147d907349bf5038898e2013c1d12411cf15802b
Afonsosequeira04 <afonsosequeira350@gmail.com>
2025-06-21 05:04:33 +0100

    kubernetes authentication

--- EKS/main.tf
+++ EKS/main.tf
@@ -28,56 +28,26 @@ module "vpc" {
 }
 
 module "eks" {
-  source  = "terraform-aws-modules/eks/aws"
-  version = "19.15.3" # Pinned exact version
+  source                         = "terraform-aws-modules/eks/aws"
+  cluster_name                   = "my-eks-cluster"
+  cluster_version                = "1.29"
+  cluster_endpoint_public_access = true
+  vpc_id                         = module.vpc.vpc_id
+  subnet_ids                     = module.vpc.private_subnets
 
-  # Cluster config
-  cluster_name    = "my-eks-cluster"
-  cluster_version = "1.29"
-  cluster_endpoint_public_access = true # Explicitly enable public access
-  
-  # Networking
-  vpc_id     = module.vpc.vpc_id
-  subnet_ids = module.vpc.private_subnets
-
-  # Node Group
   eks_managed_node_groups = {
     nodes = {
-      min_size     = 1
-      max_size     = 3
-      desired_size = 2
-      instance_types = ["t3.medium"]
-      
-      # Recommended additional node group settings
-      capacity_type = "ON_DEMAND"
-      disk_size     = 20
-      labels = {
-        role = "general"
-      }
+      min_size       = 1
+      max_size       = 3
+      desired_size   = 2
+      instance_types = var.instance_types
     }
   }
-
-  # Modern Access Control (updated policy ARN)
-  access_entries = {
-    admin-user = {
-      principal_arn = "arn:aws:iam::097648937889:user/My-Afonso-IAM-User"
-      policy_associations = {
-        full-access = {
-          policy_arn = "arn:aws:eks::aws:cluster-access-policy/AmazonEKSClusterAdminPolicy"
-          access_scope = {
-            type = "cluster"
-          }
-        }
-      }
-    }
-  }
-
   tags = {
     Environment = "dev"
     Terraform   = "true"
   }
 }
-
 
 data "aws_eks_cluster" "cluster" {
   name = module.eks.cluster_name
